f1c842461c35228bbe206d18a596996ee0666250
BavGames <83881717+BavGames@users.noreply.github.com>
2025-12-15 01:47:53 +0300

    5.0.3
    Unreal Engine Example Project 5.0.3

---
+++ Source/InteractSystemDemo/InteractSystemDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InteractSystemDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTSYSTEMDEMO_API AInteractSystemDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
